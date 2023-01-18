0169c236c49c3320b9c37e49a0520ba2552ac909
saadfrhan <saadfarhan347@gmail.com>
2023-01-18 14:34:25 +0500

    feat: minor updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import inquirer from 'inquirer';
 import inquirerPrompt from 'inquirer-autocomplete-prompt';
 import { countWords } from './word_count.js';
+import chalkAnimation from "chalk-animation";
 
 inquirer.registerPrompt('autocomplete', inquirerPrompt);
 
-await inquirer.prompt([{
-  type: 'autocomplete',
-  message: 'Please enter text:',
-  name: 'text',
-  source: countWords,
-  searchText: 'Counting...'
-}]).then(() => {
+const sleep = (ms: number = 2000) => new Promise(resolve => setTimeout(resolve, ms));
+(async () => {
+  const rainbowTitle = chalkAnimation.rainbow('Welcome to the ATM!');
+  await sleep();
+  rainbowTitle.stop();
+  await inquirer.prompt([{
+    type: 'autocomplete',
+    message: 'Please enter text:',
+    name: 'text',
+    source: countWords,
+    searchText: 'Counting...'
+  }]);
   console.log('Thanks for using this Word Counter CLI.')
-})
+})();
